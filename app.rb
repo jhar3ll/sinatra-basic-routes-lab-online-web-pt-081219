a303eab95cf2d7009d1d1bcd35fe4404e74ac332
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-03 23:59:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/name' do 
+    "My name is Jharell"
+  end 
   
+  get '/hometown' do 
+    "My hometown is Harvey"
+  end 
   
+  get '/favorite-song' do 
+    "My favorite song is Hollow Trees"
+  end 
 end
